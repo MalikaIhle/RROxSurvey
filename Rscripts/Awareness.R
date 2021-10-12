05fd6ad6cd7ef86dbb38190a957d4c5d18450c22
Malika Ihle <malika_ihle@hotmail.fr>
2021-10-12 21:25:09 +0100

    call sim data round 2 + compact prep of subdataset + create double horizontal stacked barplot per ORP

--- Rscripts/Awareness.R
+++ Rscripts/Awareness.R
@@ -3,32 +3,29 @@
 ## round 1: PGR - 12 jan 2021 to 1 march 2021 ##
 ################################################
 
-#rm(list = ls())
-#source("Rscripts/FormatPGRdata.R")
+#source("Rscripts/FormatData.R")
 
+# Variables to define
 Measures
 Awareness_columns <- c(expr(Awareness_OA), expr(Awareness_Data), expr(Awareness_Code), expr(Awareness_Materials),expr(Awareness_Preprint),expr(Awareness_Prereg),expr(Awareness_RegRep))
 Awareness_answers <- c("Practicing myself", "Accessing / using only", "Aware only",  "Not aware / not sure if applicable",  "Not applicable" )
 answers_colors <- rev(c("#ABDDA4", "#FFFFBF", "#FDAE61", "#D7191C", "black"))
-title_plot <- 'Awareness'
+#title_plot <- 'Awareness'
+title_plot <- 'PGR students awareness of ORPs'
+skeleton <- create_skeleton(Measures, Divisions, Awareness_answers, Awareness_columns) # create skeleton of all possible answers
+
+# create PGR dataset for plotting per Divisions -----
+pgrdata_Awareness <- pgrdata[, c(grep("Div", colnames(pgrdata)), grep(pattern="^Awareness", x=colnames(pgrdata)))]
+summaryitems_pgrdata_Awareness <- bind_summaries_items(Measures, pgrdata_Awareness, Awareness_columns)
+pgrdata_Awareness <- merge(skeleton, summaryitems_pgrdata_Awareness, by = "ID", all.x = TRUE) # merge summary items to skeleton
+
+# create Staff dataset for plotting per Divisions -----
+staffdata_Awareness <- staffdata[, c(grep("Div", colnames(staffdata)), grep(pattern="^Awareness", x=colnames(staffdata)))]
+summaryitems_staffdata_Awareness <- bind_summaries_items(Measures, staffdata_Awareness, Awareness_columns)
+staffdata_Awareness <- merge(skeleton, summaryitems_staffdata_Awareness, by = "ID", all.x = TRUE) # merge summary items to skeleton
 
 
-
-# create datadet for plotting per Divisions -----
-## select subdataset 
-pgrdata_Awareness <- pgrdata[pgrdata$StudentStaff == "Student",  
-                          c(grep("Div", colnames(pgrdata)), grep(pattern="^Awareness", x=colnames(pgrdata)))]
-head(pgrdata_Awareness)
-
-## create skeleton of all possible answers
-skeleton <- create_skeleton(Measures, Divisions, Awareness_answers, Awareness_columns)
-
-## summarise items                                         
-summaryitems <- bind_summaries_items(Measures, pgrdata_Awareness, Awareness_columns)
-
-## merge summary items to skeleton
-pgrdata_Awareness <- merge(skeleton, summaryitems, by = "ID", all.x = TRUE)
-rm(skeleton, summaryitems, Awareness_columns)
+rm(skeleton, summaryitems_pgrdata_Awareness, summaryitems_staffdata_Awareness, Awareness_columns)
 
 
 # plot per Division -----
@@ -46,5 +43,18 @@ All_pgrdata_Awareness <- regroup_all_data(pgrdata_Awareness)
 
 
 # Horizontal stacked barplot per ORP
-horizontal_stack_barplot_per_ORP(pgrdata_Awareness, Awareness_answers, answers_colors, title_plot)
-#ggsave("Figures/example-plot-Awareness-per-ORP.png", width = 7, height = 9, bg = "white")
+pgrdata_Awareness_perORP <- horizontal_stack_barplot_per_ORP(pgrdata_Awareness, Awareness_answers, answers_colors, title_legend = NULL, title_plot = "PGR students")
+pgrdata_Awareness_perORP
+#ggsave("Figures/example-plot-Awareness-per-ORP.png", width = 5, height = 9, bg = "white")
+
+staffdata_Awareness_perORP <- horizontal_stack_barplot_per_ORP(staffdata_Awareness, Awareness_answers, answers_colors, title_legend = NULL, title_plot = "Researchers")
+staffdata_Awareness_perORP
+
+doubleplot_Awareness <- ggarrange(pgrdata_Awareness_perORP, staffdata_Awareness_perORP, ncol=2, nrow=1, common.legend = TRUE, legend="bottom")
+annotate_figure(doubleplot_Awareness,top = text_grob("Awareness of ORPs", face = "bold", size = 14))
+
+doubleplot_Awareness <- ggarrange(pgrdata_Awareness_perORP, 
+                                  staffdata_Awareness_perORP, 
+                                  ncol=2, nrow=1, common.legend = TRUE, legend="bottom")
+doubleplot_Awareness <- annotate_figure(doubleplot_Awareness, top = text_grob("Awareness of ORPs", face = "bold", size = 14))
+#ggsave("Figures/Awareness-per-ORP.png", width = 10, height = 9, bg = "white")
