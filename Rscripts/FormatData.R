96d0c250c6aba0bbcae6216d4d065e6e2cd4dfba
Malika Ihle <malika_ihle@hotmail.fr>
2021-10-12 21:22:56 +0100

    renmae pgrdata round1_data and define pgr data the subset after removing the 40 staff answers in round 1 + make a function of data cleaning to apply to round 2 data

---
+++ Rscripts/FormatData.R
@@ -0,0 +1,34 @@
+################################################
+##    RROx survey: Open research at Oxford    ##
+## round 1: PGR - 12 jan 2021 to 1 march 2021 ##
+##                 Format data                ##
+################################################
+
+rm(list = ls())
+
+# Packages
+library(here)
+library(tidyverse)
+library(here)
+library(RColorBrewer)
+library(janitor)
+library(ggpubr)
+
+# Functions
+source(here::here("Rscripts","Functions-and-Parameters.R"))
+
+# Load data
+data_round1 <- read.csv(here("Data/RealData_20210302-0945.csv"), stringsAsFactors=FALSE)
+data_round2 <- read.csv(here("Data/SimulatedData_20201214.csv"), stringsAsFactors=FALSE) ## this is simulated data to prep the code for round 2
+surveyindex <- read.csv(here("Data/SurveyIndex.csv"), stringsAsFactors=FALSE)
+targetnumbers <- read.csv(here("Data/TargetNumbers.csv"), stringsAsFactors=FALSE) # for sample size script
+
+# Format data
+data_round1 <- clean_qualtrics_data(data_round1, surveyindex)
+data_round2 <- clean_qualtrics_data(data_round2, surveyindex) ## this is simulated data to prep the code for round 2
+rm(surveyindex)
+
+# Split data per respondent type
+pgrdata <- data_round1[data_round1$StudentStaff == "Student",]
+staffdata <- rbind(data_round1[data_round1$StudentStaff == "Staff",], data_round2)
+
