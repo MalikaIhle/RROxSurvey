71d285b74ce70f1fc9026bffcd6e97ba7de46b2a
Malika Ihle <malika_ihle@hotmail.fr>
2021-12-16 20:44:12 +0000

    sort inventory for useful entries

--- Rscripts/Inventory.R
+++ Rscripts/Inventory.R
@@ -5,15 +5,24 @@
 pgrdata_Inventory <- pgrdata[,grep(pattern="^Inventory", x=colnames(pgrdata))]
 pgrdata_Inventory <- data.frame(stack(pgrdata_Inventory))$values
 pgrdata_Inventory <- pgrdata_Inventory[!is.na(pgrdata_Inventory)]
+pgrdata_Inventory <- data.frame(initiative = toupper(pgrdata_Inventory))
+pgrdata_Inventory$initiative_cat <- NA
+pgrdata_Inventory$initiative_cat[str_detect(pgrdata_Inventory$initiative, 
+                                        c("T THINK|NO IDEA|T KNOW|NOT AWARE|NOT SURE"))] <- 'Not categorised'
+
+pgrdata_Inventory$initiative[!is.na(pgrdata_Inventory$initiative_cat)]
+pgrdata_Inventory$initiative[is.na(pgrdata_Inventory$initiative_cat)]
+pgrdata_Inventory <- pgrdata_Inventory[is.na(pgrdata_Inventory$initiative_cat),]
 
 ## Nb of responses
-length(pgrdata_Inventory)
+nrow(pgrdata_Inventory)
+pgrdata_Inventory <- data.frame(pgrdata_Inventory)
 
-
-# staffdata_Inventory
-staffdata_Inventory <- staffdata[,grep(pattern="^Inventory", x=colnames(staffdata))]
-staffdata_Inventory <- data.frame(stack(staffdata_Inventory))$values
-staffdata_Inventory <- staffdata_Inventory[!is.na(staffdata_Inventory)]
+# allstaffdata_Inventory
+allstaffdata_Inventory <- allstaffdata[,grep(pattern="^Inventory", x=colnames(allstaffdata))]
+allstaffdata_Inventory <- data.frame(stack(allstaffdata_Inventory))$values
+allstaffdata_Inventory <- allstaffdata_Inventory[!is.na(allstaffdata_Inventory)]
+allstaffdata_Inventory <- data.frame(initiative = toupper(allstaffdata_Inventory))
 
 ## Nb of responses
-length(staffdata_Inventory)
+nrow(allstaffdata_Inventory)
